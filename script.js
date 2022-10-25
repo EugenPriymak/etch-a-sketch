caa51afad5971bd3c0e6bcefa71393e705525e08
Eugen Priymak <epriymak1@gmail.com>
2022-10-25 11:36:39 +0200

    add grid 16x16 instead of flex

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ let mainDiv = document.querySelector('.main');
 
 for (i=0; i<256; i++) {
     let div = document.createElement('div');
-    div.style.cssText = 'border: 1px solid black; height: 48px; width: 48px;';
+    div.classList.add('grid-div');
+    div.style.cssText = 'border: 1px solid black; height: 20px; width: 20px;';
     mainDiv.appendChild(div);
 }
