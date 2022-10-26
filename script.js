0878f3761f95470a2f8f35db896257c2ef64af5c
Eugen Priymak <epriymak1@gmail.com>
2022-10-26 11:49:28 +0200

    edit createBoard()

--- script.js
+++ script.js
@@ -1,10 +1,26 @@
-let mainDiv = document.querySelector('.main');
+let mainDiv = document.querySelector('.main-board');
+let btnInput = document.querySelector('.btn-input');
+let mainBoard = document.querySelector('.main-board');
+// btnInput.addEventListener('click', createBoard);
+let num;
 
-for (i=0; i<256; i++) {
-    let div = document.createElement('div');
-    div.classList.add('grid-div');
-    div.style.cssText = 'border: 1px solid black; height: 20px; width: 20px;';
-    mainDiv.appendChild(div);
-    div.addEventListener('mouseover' , () => div.style.backgroundColor = 'red');
+function createBoard(num) {
+    console.log("input");
+    mainBoard.style.gridTemplateRows = `repeat(${num}, 1fr)`;
+    mainBoard.style.gridTemplateColumns =  `repeat(${num}, 1fr)`;
+    let numDivs = num * num;
+    for (i=0; i< numDivs; i++) {
+        let div = document.createElement('div');
+        div.classList.add('grid-div');
+        div.style.cssText = 'border: .1px solid black';
+        mainDiv.appendChild(div);
+        div.addEventListener('mouseover' , () => div.style.backgroundColor = 'red');
+    }    
 }
+
+createBoard(100);
+
+
+
+
 
